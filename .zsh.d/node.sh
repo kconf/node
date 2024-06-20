ce4a9fe78b319c2556b6eee9a1a80aaba88a8c72
Jianwei Han <hanjianwei@gmail.com>
2024-06-20 10:38:23 +0800

    Update

--- .zsh.d/node.sh
+++ .zsh.d/node.sh
@@ -1,3 +1,3 @@
-export NODE_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
+export N_NODE_MIRROR=https://npmmirror.com/mirrors/node
 export N_PREFIX="$HOME/n"
-[[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+path=("$N_PREFIX/bin" $path)
