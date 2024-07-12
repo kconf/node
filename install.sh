570698b81eda9a2f95d2e270014c79104699394f
Jianwei Han <hanjianwei@gmail.com>
2024-07-12 20:17:41 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ export N_PREFIX="$HOME/Applications/n"
 export N_NODE_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
 
 if [ ! -f $N_PREFIX/bin/n ]; then
-  curl -L https://raw.githubusercontent.com/mklement0/n-install/stable/bin/n-install | bash
+  ./n-install/bin/n-install
 fi
